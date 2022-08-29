cc9e32adfd54a9ca88251b2b2c01b127dcea9b47
Johnny <johnnyw827@gmail.com>
2022-08-29 13:14:40 -0400

    testing again

--- ssh.py
+++ ssh.py
@@ -1,1 +1,13 @@
-print('hi')
+from pexpect import pxssh
+import getpass
+
+try:
+    s = pxssh.pxssh()
+    hostname = input('hostname: ')
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+    s.login(hostname, username, password)
+
+except pxssh.ExceptionPxssh as e:
+    print("pxssh failed on login.")
+    print(e)
